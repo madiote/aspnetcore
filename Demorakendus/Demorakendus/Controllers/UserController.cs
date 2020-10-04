2701f2756130f01c91e6e602454916dcc4f22f47
Madis <43170891+madiote@users.noreply.github.com>
2020-10-04 18:07:35 +0300

    MVC mudelid

---
+++ Demorakendus/Demorakendus/Controllers/UserController.cs
@@ -0,0 +1,23 @@
+﻿using Demorakendus.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Demorakendus.Controllers
+{
+    public class UserController
+    {
+        public ObjectResult Index()
+        {
+            var user = new User
+            { 
+                ID = 1, 
+                FirstName = "John",
+                LastName = "Smith"
+            };
+            return new ObjectResult(user);
+        }
+    }
+}
