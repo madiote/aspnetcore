dd2166535a4cc0fb6e4776b19343a2fa589a687f
Madis <43170891+madiote@users.noreply.github.com>
2020-10-04 17:12:07 +0300

    Model-View-Controller

--- Demorakendus/Demorakendus/Startup.cs
+++ Demorakendus/Demorakendus/Startup.cs
@@ -24,6 +24,7 @@ namespace Demorakendus
 
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddControllers(options => options.EnableEndpointRouting = false);
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -35,13 +36,16 @@ namespace Demorakendus
             }
 
             app.UseRouting();
+            app.UseMvcWithDefaultRoute();
 
+            /*
             app.UseFileServer();
-
+            
             app.UseEndpoints(endpoints =>
             {
-                
+               
             });
+            */
         }
     }
 }
