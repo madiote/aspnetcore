f46eb19861db7b860acd39e0f234c6b58a47e8ae
Madis <43170891+madiote@users.noreply.github.com>
2020-10-04 15:16:02 +0300

    Seadistusfail

--- Demorakendus/Demorakendus/Startup.cs
+++ Demorakendus/Demorakendus/Startup.cs
@@ -7,6 +7,7 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Configuration;
 
 namespace Demorakendus
 {
@@ -14,6 +15,13 @@ namespace Demorakendus
     {
         // This method gets called by the runtime. Use this method to add services to the container.
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
+        private IConfiguration Configuration;
+
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
         public void ConfigureServices(IServiceCollection services)
         {
         }
@@ -32,7 +40,8 @@ namespace Demorakendus
             {
                 endpoints.MapGet("/", async context =>
                 {
-                    await context.Response.WriteAsync("Tere maailm!");
+                    var muutuja = Configuration["MinuMuutuja"];
+                    await context.Response.WriteAsync("Tere maailm! " + muutuja);
                 });
             });
         }
