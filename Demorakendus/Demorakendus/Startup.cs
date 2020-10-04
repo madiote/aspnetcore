683c26508bc6aa0bdf13c8a71e39590079a4c091
Madis <43170891+madiote@users.noreply.github.com>
2020-10-04 14:44:13 +0300

    Muuda sõnumit

--- Demorakendus/Demorakendus/Startup.cs
+++ Demorakendus/Demorakendus/Startup.cs
@@ -32,7 +32,7 @@ namespace Demorakendus
             {
                 endpoints.MapGet("/", async context =>
                 {
-                    await context.Response.WriteAsync("Hello World!");
+                    await context.Response.WriteAsync("Tere maailm!");
                 });
             });
         }
